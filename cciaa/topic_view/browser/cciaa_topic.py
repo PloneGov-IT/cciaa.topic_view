7be9c8c71d63e6da8493997eb814b0ba225d1994
Andrea Cecchi <andrea.cecchi85@gmail.com>
2009-06-23 12:27:45 +0000

    risolto bug nella vista (#13)

--- cciaa/topic_view/browser/cciaa_topic.py
+++ cciaa/topic_view/browser/cciaa_topic.py
@@ -1,18 +1,8 @@
 from Products.Five import BrowserView
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-
-from Products.CMFCore.utils import getToolByName
-
 
 class TopicView(BrowserView):
     """Vista dei cercatori"""
     IMG_TAG = u"""<img src="%s" alt="%s" title="%s" />"""
-    
-    def getDescr(self, doc_id):
-        """restituisce la descrizione dell'oggetto"""
-        context = self.context
-        page = getattr(context, doc_id)
-        return page.Description()
     
     def generateImgTag(self, icon, alt="", title=""):
         """Dato un'icona caricata dal tramite @@plone.getIcon ne genera un tag (X)HTML da usare
