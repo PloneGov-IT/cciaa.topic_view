7f347290cf9f6eb0a43ccb5b3ec1f706ab4a0c68
Luca Fabbri <luca@keul.it>
2009-07-13 10:12:49 +0000

    Fissato il formato dell'egg

--- setup.py
+++ setup.py
@@ -8,36 +8,15 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.0.6'
-
-long_description = (
-    read('README.txt')
-    + '\n' +
-    'Change history\n'
-    '**************\n'
-    + '\n' +
-    read('docs/HISTORY.txt')
-    + '\n' +
-    'Detailed Documentation\n'
-    '**********************\n'
-    + '\n' +
-    read('cciaa', 'topic_view', 'README.txt')
-    + '\n' +
-    'Contributors\n'
-    '************\n'
-    + '\n' +
-    read('CONTRIBUTORS.txt')
-    + '\n' +
-    'Download\n'
-    '********\n'
-    )
+version = '1.0.7'
 
 tests_require=['zope.testing']
 
 setup(name='cciaa.topic_view',
       version=version,
       description="",
-      long_description=long_description,
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         'Framework :: Plone',
