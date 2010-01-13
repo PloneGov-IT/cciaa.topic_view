024ef74211228cf61a6e2ebbc18c2f294c2e02c5
luca.fabbri <luca.fabbri@674e4dab-9b64-4953-9e98-fc092439839e>
2010-01-13 16:23:49 +0000

    Rifaccio il tag

--- C3P/cciaa.topic_view/tags/1.0.7/cciaa/modulistica/interfaces.py
+++
@@ -1,9 +0,0 @@
-from zope import schema
-from zope.interface import Interface
-
-from zope.app.container.constraints import contains
-from zope.app.container.constraints import containers
-
-from cciaa.modulistica import modulisticaMessageFactory as _
-
-# -*- extra stuff goes here -*-
