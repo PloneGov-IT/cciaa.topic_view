ff1a483add710737feea14382bfbe69a2f2949ae
luca.fabbri <luca.fabbri@674e4dab-9b64-4953-9e98-fc092439839e>
2010-04-15 14:33:47 +0000

    rifaccio il tag

--- C3P/cciaa.topic_view/tags/1.0.8/cciaa/topic_view/interfaces.py
+++
@@ -1,9 +0,0 @@
-from zope import schema
-from zope.interface import Interface
-
-from zope.app.container.constraints import contains
-from zope.app.container.constraints import containers
-
-from cciaa.topic_view import topic_viewMessageFactory as _
-
-# -*- extra stuff goes here -*-
