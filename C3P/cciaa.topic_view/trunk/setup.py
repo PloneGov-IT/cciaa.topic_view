22c076b291a90a62e438ee8fc27ab6fa002c63eb
keul <keul@674e4dab-9b64-4953-9e98-fc092439839e>
2013-12-16 10:33:23 +0000

    Preparing release 1.1.0

--- C3P/cciaa.topic_view/trunk/setup.py
+++ C3P/cciaa.topic_view/trunk/setup.py
@@ -5,7 +5,7 @@ This module contains the tool of cciaa.topic_view
 import os
 from setuptools import setup, find_packages
 
-version = '1.1.0dev'
+version = '1.1.0'
 
 tests_require=['zope.testing']
 
